be8d26ee617c0e4ae8ec58cc10e3433d6362f17c
File Explorer App <app@example.com>
2026-02-11 00:21:58 +0200

    تحميل app/src/main/java/awab/quran/ar/ui/theme/Color.kt

---
+++ app/src/main/java/awab/quran/ar/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package awab.quran.ar.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// ألوان التطبيق الإسلامية
+val QuranGreen = Color(0xFF2E7D32)
+val QuranGreenLight = Color(0xFF66BB6A)
+val QuranGold = Color(0xFFD4AF37)
+val QuranDarkGreen = Color(0xFF1B5E20)
+val QuranBackground = Color(0xFFF5F5F5)
+val QuranWhite = Color(0xFFFFFFFF)
+val QuranGray = Color(0xFF757575)
+val QuranLightGray = Color(0xFFE0E0E0)
